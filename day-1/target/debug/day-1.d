4cd08011c77929cb69d4750d824f998b773300a2
Martin <martin.hogstedt@hotmail.com>
2022-12-02 17:27:30 +0100

    removed files

--- day-1/target/debug/day-1.d
+++
@@ -1,1 +0,0 @@
-/home/martin/advent-of-code-2022/day-1/target/debug/day-1: /home/martin/advent-of-code-2022/day-1/src/main.rs
--- day-1/target/debug/deps/day_1-0f1f64690cb28103.d
+++
@@ -1,5 +0,0 @@
-/home/martin/advent-of-code-2022/day-1/target/debug/deps/day_1-0f1f64690cb28103: src/main.rs
-
-/home/martin/advent-of-code-2022/day-1/target/debug/deps/day_1-0f1f64690cb28103.d: src/main.rs
-
-src/main.rs:
