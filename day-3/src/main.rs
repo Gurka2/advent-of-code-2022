0cae2576ff04146b5da6c066f2fdc93e761735ee
Martin <martin.hogstedt@hotmail.com>
2022-12-04 13:59:41 +0100

    finished part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -10,17 +10,38 @@ const LOWER: u32 = 96;
 fn main() {
 
     let mut sum_p1: u32 = 0;
+    let mut sum_p2: u32 = 0;
+
+    let mut counter: i32 = 0;
+
+    let mut string1: String = "".to_string();
+    let mut string2: String = "".to_string();
+    let mut string3: String = "".to_string();
 
     // Parse the imput file
     if let Ok(lines) = read_lines(INPUT) {
         for line in lines {
             if let Ok(ip) = line {
+
                 sum_p1 += part_1(&ip);
+
+                match counter {
+                    0 => string1 = ip,
+                    1 => string2 = ip,
+                    2 => string3 = ip,
+                    _ => println!("Something is wrong")
+                }
+                counter += 1;
+                if counter == 3 {
+                    sum_p2 += part_2(&string1, &string2, &string3);
+                    counter = 0;
+                }
             }
         }
     }
 
     println!("Part 1: {}", sum_p1);
+    println!("Part 2: {}", sum_p2);
 }
 
 
@@ -30,52 +51,63 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+fn part_2(string1: &str, string2: &str, string3: &str) -> u32 {
+
+    let duplicates: String = get_duplicate_char(string1, string2);
+    let duplicates_final: String = get_duplicate_char(&duplicates, string3);
+    return get_priority(duplicates_final);
+
+}
+
 
 fn part_1(string: &str) -> u32 {
 
-    let mut priority: u32 = 0;
-    let result = get_duplicate_char(string);
+    let len = string.len();
+    let string1 = &string[0..len/2];
+    let string2 = &string[len/2..];
 
-    if result.is_some() {
-        priority = result.unwrap();
-        
-        if priority > 96 {
-            priority -= LOWER;
-        } 
+    let result = get_duplicate_char(string1, string2);
+    return get_priority(result);
+}
 
-        else {
-            priority -= UPPER;
-        }
+fn get_priority(s: String) -> u32 {
 
-    }
+    let mut priority = s.chars().nth(0).unwrap() as u32;
     
+    if priority > 96 {
+        priority -= LOWER;
+    } 
+
+    else {
+        priority -= UPPER;
+    }
+
     return priority;
 }
 
 
-fn get_duplicate_char(string: &str) -> Option<u32> {
+fn get_duplicate_char(string1: &str, string2: &str) -> String {
+    // Returns all unique duplicates in the strings
 
+    let mut duplicates = String::from("");
     let mut items: HashMap<char, u32> = HashMap::new();
-    let mut result: Option<u32> = None;
+    let mut result: Option<u32>;
 
-    let length: usize = (string.len())/2;
+    for (_i, c) in string1.chars().enumerate() {
 
-    for (i, c) in string.chars().enumerate() {
+        items.insert(c, c as u32);
+    }
 
-        // Iterating over first half
-        if i < length {
-            items.insert(c, c as u32);
-        } 
+    // Look for chars in the hashmap (inte drogen) 
+    for (_i, c) in string2.chars().enumerate() {
 
-        // Look for chars in the hashmap (inte drogen) 
-        else {
+        result = items.get(&c).copied();
 
-            result = items.get(&c).copied();
-            if result.is_some() {
-                return result;
-            }
+        if result.is_some() && !duplicates.contains(*&c) {
+            duplicates.push(c);
         }
     }
-    return result;
+
+    return duplicates;
 
 }
